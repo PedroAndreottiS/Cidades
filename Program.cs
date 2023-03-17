149a26ad56317cb6200604ba1530c07050e4096e
DESKTOP-V0H394L\User <e107ti@etec.sp;gov;br>
2023-03-17 20:08:28 -0300

    exercício concluído

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿
+Console.WriteLine("Cidades do Litoral");
+Console.WriteLine("1.Mongaguá");    
+Console.WriteLine("2.peruíbe");
+Console.WriteLine("3.Itanhaém");
+Console.WriteLine("4.Cananéia");
+Console.WriteLine("5.Iguape");
+Console.WriteLine("6.Ilha Comprida");
+
